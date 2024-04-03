1e6d61c6204ed9380d34356f131046e2c6ab29a5
FrenDithachai <dithachai.fren@gmail.com>
2024-04-04 00:37:16 +0700

    convert-Funtion

---
+++ FuntionPeechaiyo.js
@@ -0,0 +1,34 @@
+//? 1. ให้แปลงจาก Function ปกติไปเป็น Arrow Function
+/*function fullname(firstname, lastname) {
+    return firstname + lastname;
+}*/
+
+//* fullname=(firstname, lastname)=>firstname+lastname;
+
+//?. ให้แปลงจาก Function ปกติไปเป็น Arrow Function
+
+/*function triangleArea(base, height) {
+    console.log("Calculate triangle area");
+    const area = (base * height) / 2;
+    return area;
+}*/
+
+//*แปลง
+/*triangleArea=(base, height)=>{
+    console.log("Calculate triangle area",base,height);
+    const area = (base * height) / 2;
+    console.log(area);
+}
+let num= triangleArea(50,50)*/
+
+//? 3. ให้แปลงจาก Arrow Function ไปเป็น Function ปกติ
+// const circleArea = (radius) => Math.PI * radius ** 2;
+
+//*แปลง
+function circleArea(radius){
+    return Math.PI * radius ** 2
+    //console.log(Math.PI * radius ** 2);
+    
+    
+}
+let number = circleArea(20);
